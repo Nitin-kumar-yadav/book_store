76a257535ea9af6b14d4f28f328e9e8902cb1607
Nitin yadav <77772979+Nitin-kumar-yadav@users.noreply.github.com>
2024-05-19 18:53:48 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -22,7 +22,7 @@ const CreateBooks = () => {
             publishYear,
         };
         setLoading(true);
-        axios.post('https://book-store-api-nine.vercel.app/books', data)
+        axios.post('https://book-store-api-six.vercel.app/books', data)
             .then((response) => {
                 setLoading(false);
                 enqueueSnackbar('Book Created Successfully', { variant: 'success' });
